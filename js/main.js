eae8a282f667ec4c94ea1f47a4c1710e18bdc217
hsingh <hsingh@casenetllc.com>
2020-10-28 12:44:40 -0400

    nav active class added

--- js/main.js
+++ js/main.js
@@ -8,5 +8,15 @@ function onReady() {
     }, !1);
     var e = document.querySelector && document.querySelector("html");
     e && (e.className += " " + platformClassName)
+
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
+    });
+
+    var $lis = $(".navigation-extendable ul li").click(function (e) {
+        $lis.removeClass('current');
+        $(this).addClass('current');
+    })
 }
 document.addEventListener("DOMContentLoaded", onReady);
